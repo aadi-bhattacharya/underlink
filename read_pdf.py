35c977c47a24ef1d733f85e0b6c10b938e52f059
aadi bhattacharya <aadiforacads@gmail.com>
2026-03-01 12:58:04 +0530

    scrubbed.

--- read_pdf.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfReader
-
-reader = PdfReader("c:/Users/varun/Downloads/Hackenza_Problem_statement_3-1.pdf")
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-
-with open("c:/Users/varun/Desktop/Hackenza-underLink/underlink/out.txt", "w", encoding="utf-8") as f:
-    f.write(text)
